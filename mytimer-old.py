5e0c31b5dadccecf000c5424bd6b1a7afd4b0d36
U-gsy-PC\gsy <foundergsy@163.com>
2018-11-22 17:47:44 +0800

    update

---
+++ mytimer-old.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import time
+
+reps = 1000
+repslist = range(reps)
+
+def timer(func, *pargs, **kargs):
+    start = time.clock()
+    for i in repslist:
+        ret = func(*pargs, **kargs)
+    elapsed = time.clock() - start
+    return(elapsed, ret)
+    
